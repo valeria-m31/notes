71d4fd91569e79d88d1db58ee25e6d8f9e327335
Valerym31 <valeria.evg.chernysheva@gmail.com>
2019-11-20 16:22:21 +0300

    added files

---
+++ backend/notes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import NoteViewSet
+
+
+router = routers.DefaultRouter()
+router.register('notes', NoteViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
